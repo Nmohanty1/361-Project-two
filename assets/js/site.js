f805f031c19612be65beb0da3f09c51d86daeb74
Nikhil Mohanty <nmohanty1@hawk.iit.edu>
2018-12-06 16:08:49 -0600

    fixing buttons

--- assets/js/site.js
+++ assets/js/site.js
@@ -15,19 +15,19 @@ buttonN.addEventListener('click', function(){
   console.log(f1);
   if (f1 === 1){
     document.getElementById('family4').style.display = "none";
-    document.getElementById('family1').style.display = "block";
+    document.getElementById('family1').style.display = "flex";
   };
   if(f1===2){
     document.getElementById('family1').style.display = "none";
-    document.getElementById('family2').style.display = "block";
+    document.getElementById('family2').style.display = "flex";
   };
   if(f1===3){
     document.getElementById('family2').style.display = "none";
-    document.getElementById('family3').style.display = "block";
+    document.getElementById('family3').style.display = "flex";
   };
   if(f1===4){
     document.getElementById('family3').style.display = "none";
-    document.getElementById('family4').style.display = "block";
+    document.getElementById('family4').style.display = "flex";
   };
 });
 
