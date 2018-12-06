2fe25b2583379926ec505097f8907564a46bfc7e
Nikhil Mohanty <nmohanty1@hawk.iit.edu>
2018-12-05 21:25:39 -0600

    javascript image slideshow

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,13 +2,55 @@ document.addEventListener('DOMContentLoaded' , function() {
   console.log("Bear Down");
 });
 
+var buttonN =document.querySelector('#next');
+var buttonB =document.querySelector('#prev');
 
-$("next").click(function()){
-$('#slides').append($("#slides img:first-of-type"));
+var f1 = 1;
+
+buttonN.addEventListener('click', function(){
+  f1++;
+  if (f1 >= 5) {
+    f1 = 1;
+  };
+  console.log(f1);
+  if (f1 === 1){
+    document.getElementById('family4').style.display = "none";
+    document.getElementById('family1').style.display = "block";
+  };
+  if(f1===2){
+    document.getElementById('family1').style.display = "none";
+    document.getElementById('family2').style.display = "block";
+  };
+  if(f1===3){
+    document.getElementById('family2').style.display = "none";
+    document.getElementById('family3').style.display = "block";
+  };
+  if(f1===4){
+    document.getElementById('family3').style.display = "none";
+    document.getElementById('family4').style.display = "block";
+  };
 });
 
-
-$("prev").click(function()){
-
-$("slides").prepend($("#slides img:last-of-type")
-}
+buttonB.addEventListener('click', function(){
+f1--;
+if (f1 <= 0) {
+  f1 = 4;
+};
+console.log(f1);
+if (f1 === 1){
+  document.getElementById('family2').style.display = "none";
+  document.getElementById('family1').style.display = "block";
+};
+if(f1===2){
+  document.getElementById('family3').style.display = "none";
+  document.getElementById('family2').style.display = "block";
+};
+if(f1===3){
+  document.getElementById('family4').style.display = "none";
+  document.getElementById('family3').style.display = "block";
+};
+if(f1===4){
+  document.getElementById('family1').style.display = "none";
+  document.getElementById('family4').style.display = "block";
+};
+});
